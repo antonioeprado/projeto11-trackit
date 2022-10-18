f3e7079eaca3ee56eb246dba3a1d6fc2e860c0ff
Antonio Prado <antonioeprado18@gmail.com>
2022-10-18 14:03:35 -0300

    Páginda de cadastro feita

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import HomePage from '../pages/HomePage';
+import SignUpPage from '../pages/SignUpPage';
 import logo from '../static/media/imgs/logo.svg';
 import { GlobalStyles } from '../static/styles/GlobalStyles';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <>
+    <Router>
       <GlobalStyles />
-      <HomePage />
-    </>
+      <Routes>
+        <Route path='/' element={<HomePage />}></Route>
+        <Route path='/cadastro' element={<SignUpPage />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/SignUpPage.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import styled from 'styled-components';
+
+import { SignButton } from '../static/styles/Buttons';
+import { FlexWrapperColumn, StyledForm } from '../static/styles/Wrappers';
+import { mainColor } from '../static/styles/Colors';
+import { StyledInput } from '../static/styles/Input';
+import logo from '../static/media/imgs/Group_8.png';
+
+
+
+export default function SignUpPage() {
+    return (
+        <FlexWrapperColumn>
+            <BigLogo img={logo} />
+            <StyledForm>
+                <StyledInput type="email" placeholder='email' required />
+                <StyledInput type="password" placeholder='senha' required />
+                <StyledInput type="text" placeholder='nome' required />
+                <StyledInput type="url" placeholder='foto' required />
+                <SignButton>Cadastrar</SignButton>
+            </StyledForm>
+            <SignUpLink>Já tem uma conta? Faça login!</SignUpLink>
+        </FlexWrapperColumn>
+    )
+}
+
+const BigLogo = styled.div`
+    width: 180px;
+    height: 178px;
+    background: ${props => `url(${props.img}) no-repeat center/180px 178px`};
+    margin: auto;
+    margin-top: 68px;
+    margin-bottom: 32px;
+`;
+
+const SignUpLink = styled.a`
+    font-size: 14px;
+    line-height: 17px;
+    text-align: center;
+    color: ${mainColor};
+    text-decoration-line: underline;
+    margin: auto;
+    margin-top: 25px;
+`;
